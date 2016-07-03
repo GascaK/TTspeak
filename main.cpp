2304901eca4099b59684b4d2c3f17ffd1fe9dcc9
draftjoker <gasca.kevin.a@gmail.com>
2016-07-02 19:30:57 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-//#define _ATL_APARTMENT_THREADED
-
-
 #include <iostream>
 #include <string>
+// ttspeak header file. Include in your local path.
 #include <ttspeak.h>
 
 using namespace std;
@@ -10,10 +8,14 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	string text = "";
-	ttspeak speechFemale = { 256, ttspeak::FEMALE };
-	ttspeak speechMale = { 256, ttspeak::MALE };
+	// ttspeak constructor of size 256 and voice of type
+	// female or male.
+	ttspeak speechFemale = { 100, ttspeak::FEMALE };
+	ttspeak speechMale = { 100, ttspeak::MALE };
 	do {
+		// input and assignment.
 		getline(cin, text);
+		
 		speechFemale.comSpeak(text);
 		getline(cin, text);
 		speechMale.comSpeak(text);
