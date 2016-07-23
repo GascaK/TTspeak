9738347b0416b5f03f40956ff67911990cb040c9
draftjoker <gasca.kevin.a@gmail.com>
2016-07-22 17:58:59 -0700

    Updated with Speech Recognition
    Included functions with speech recognition and further improved class instantiation.

--- ttspeak.h
+++ ttspeak.h
@@ -1,23 +1,26 @@
-/***************************************************************** 
+/********************************************************************** 
 ttspeak.h Information:
 	This header allows for use of Microsoft Speech API (SAPI)
 	without the convuluted use of their function calls. ttspeak
 	initializes the COM objects and sets up the API for ease of
 	use, for people who just want a quick and easy way to get 
-	Text-to-Speech into their C++ applications.
-*******************************************************************/
+	Text-to-Speech and Voice Recognition into their applications.
+Language:  C++ 14+
+Revision:  2.0.0 (7.22.16)
+Author:	   Gasca, K.
+************************************************************************/
 
-// COM include and _Module loader sphelper has deprecated functions
-// disabled warning 4996 for tempory solution.
+// COM include and _Module loader. sphelper has deprecated functions
+// disabled warning 4996 for temporary solution.
 // TODO: Make Microsoft(c) update sphelper.h..
 #pragma warning (disable:4996)
 #include <atlbase.h>
 #include <atlcom.h>
-#include <sphelper.h>
 #include <sapi.h>
+#include <sphelper.h>
 extern CComModule _Module;
 
-//* Main ttspeak class with constructor and self cleaning destructor.
+// Main ttspeak class with constructor and destructor.
 class ttspeak
 {
 private:
@@ -26,28 +29,53 @@ private:
 	LPWSTR ptr = NULL;
 	HRESULT hr = NULL;
 	ISpVoice* pVoice = nullptr;
-	ISpObjectToken* objTok = nullptr;
+	CComPtr<ISpRecognizer>	cpRecog;
+	CComPtr<ISpObjectToken> objTok;
+	CComPtr<ISpRecoContext> recoCon;
+	CComPtr<ISpRecoGrammar> recoGram;
+	CComPtr<ISpRecoResult>  recoRes;
 	void checkOut(const HRESULT&);
 public:
-	enum gender {MALE, FEMALE};
-	ttspeak(int in, gender);
+	ttspeak();
 	~ttspeak();
+	enum gender { MALE, FEMALE };
+	void loadComSpeak(int inArrSize, gender);
 	void comSpeak(std::string);
+	void loadVoiceInput();
+	int voiceInput(std::string& saved);
 };
 
-/* Constuctor with two paramaters, integer inArrSize and public enum
-	with identifier Gender of type int.                              */
-ttspeak::ttspeak(int inArrSize, ttspeak::gender newGender)
+/* Constructor
+		Takes in no arguments only sets up the COM objects */
+ttspeak::ttspeak()
+{
+	// Check for COM object Initialization failure. Throws exception with
+	// output of error.
+	if (FAILED(::CoInitialize(NULL)))
+		throw std::exception("Unable to Initilize COM object.");
+}
+
+// Destructor for class ttspeak. COM object unitialize. Releases SAPI's 
+// object names and sets class pointer to NULL. Deletes allocated memory
+// in heap. Programmer may call to delete that ttspeak instance they created.
+ttspeak::~ttspeak()
+{
+	::CoUninitialize();
+	if(pVoice != NULL)
+		pVoice->Release();
+	pVoice = NULL;
+	delete[] localText;
+	return;
+}
+
+/* Load function with two paramaters, integer inArrSize and
+    public enum with identifier Gender of type int.      */
+void ttspeak::loadComSpeak(int inArrSize, ttspeak::gender newGender)
 {
 	// MAX_ARR_SIZE is defined as the maximum input string literal your
 	// users can input. Users will be unable to exceed this length.
 	MAX_ARR_SIZE = inArrSize;
 	localText = new wchar_t[MAX_ARR_SIZE];
-
-	// Check for COM object Initialization failure. Throws exception with
-	// output of error.
-	if (FAILED(::CoInitialize(NULL)))
-		throw std::exception("Unable to Initialize COM object.");
 
 	// Instance creation for SpVoice (SAPI).
 	hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, 
@@ -68,26 +96,15 @@ ttspeak::ttspeak(int inArrSize, ttspeak::gender newGender)
 	}
 	
 	// Sends objTok to SetVoice (SAPI) which changes the gender of the voice
-	// output to desired gender.
+	// output to desired gender if located in registry.
 	// TODO: create more options for different voice casts. ie) age, speed 
 	//		 and setdefaults.
 	hr = pVoice->SetVoice(objTok);
 	checkOut(hr);
 }
 
-// Destructor for class ttspeak. COM object unitialize. Releases SAPI's 
-// object names and sets pointer to NULL. Deletes allocated memory in heap.
-ttspeak::~ttspeak()
-{
-	::CoUninitialize();
-	pVoice->Release();
-	pVoice = NULL;
-	delete[] localText;
-	return;
-}
-
-// Main interface class for ttspeak. One parameter of type string. Will out
-// put the string literal as Speech from default AUDIO source.
+// Main interface function for speech output. One parameter of type string. 
+//	Will output the string literal as Speech from DEFAULT audio source.
 void ttspeak::comSpeak(std::string incText)
 {
 	// Check for string input LENGTH >= MAX_ARR_SIZE then returns if failed.
@@ -101,61 +118,136 @@ void ttspeak::comSpeak(std::string incText)
 	// accepts LPWSTR as input.
 	mbstowcs(localText, incText.c_str(), strlen( incText.c_str() ) +1 );
 	ptr = localText;
-	// Output of voice with possible XML-like commands. Search MSDN SAPI doc
-	// umentation for further.
+	// Output of voice with possible XML-like commands. Search MSDN SAPI 
+	// documentation for further.
 	hr = pVoice->Speak(ptr, SPF_IS_XML, NULL);
 	checkOut(hr);
 
 	return;
 }
 
+// Load function for Speech Recognition with default Inputs and default 
+// language set to 409 | English. Other languages to come? No..
+void ttspeak::loadVoiceInput()
+{
+	// CreateInstance with a Shared Recognizer. This recognizer is the
+	// same recognizer other applications can use at the same time.
+	hr = cpRecog.CoCreateInstance(CLSID_SpSharedRecognizer);
+	checkOut(hr);
+	// Finds the best recognizer of language 409 | English
+	hr = SpFindBestToken(SPCAT_RECOGNIZERS, L"language=409", NULL, &objTok);
+	checkOut(hr);
+	// Sets the Recognizer to the Object Token
+	hr = cpRecog->SetRecognizer(objTok);
+	checkOut(hr);
+	// Activates the recognition engine state
+	hr = cpRecog->SetRecoState(SPRST_ACTIVE);
+	checkOut(hr);
+	// Creates the Recognition engine context
+	hr = cpRecog->CreateRecoContext(&recoCon);
+	checkOut(hr);
+	// Sets the Event Handle as Console Window Event
+	hr = recoCon->SetNotifyWin32Event();
+	checkOut(hr);
+	// Sets the Engine interests as Speech Recognized.
+	hr = recoCon->SetInterest(SPFEI(SPEI_RECOGNITION), SPFEI(SPEI_RECOGNITION));
+	checkOut(hr);
+	// Creates the grammar that is recognizable
+	hr = recoCon->CreateGrammar(0, &recoGram);
+	checkOut(hr);
+	// Loads the diction that it can recognize
+	hr = recoGram->LoadDictation(NULL, SPLO_STATIC);
+	checkOut(hr);
+	// sets the diction state to active for input
+	hr = recoGram->SetDictationState(SPRS_ACTIVE);
+	checkOut(hr);
+
+	return;
+}
+
+// Main interface for speech recognition, takes string address and returns
+// value as type string.
+int ttspeak::voiceInput(std::string& saved)
+{
+	// handle for speech recognized events.
+	HANDLE hSpeechEvent = recoCon->GetNotifyEventHandle();
+	// returns 1 if invalid handle was passed through EventHandle()
+	if (hSpeechEvent == INVALID_HANDLE_VALUE)
+		return 1;
+	// Sets up the Events and the Wait function.
+	CSpEvent spEvent;
+	LPWSTR coMemResult = NULL;
+	HANDLE rghEvents[] = { hSpeechEvent };
+	DWORD dMessage = WaitForMultipleObjects(sp_countof(rghEvents),
+		rghEvents, FALSE, INFINITE);
+
+	// If wait was successful 
+	if (dMessage == WAIT_OBJECT_0)
+	{
+		// Get the Event
+		spEvent.GetFrom(recoCon);
+		// Set Event
+		recoRes = spEvent.RecoResult();
+		// Get text from recognized string
+		hr = recoRes->GetText(SP_GETWHOLEPHRASE, SP_GETWHOLEPHRASE,
+			TRUE, &coMemResult, NULL);
+		checkOut(hr);
+	}
+	// Place holder for string return
+	char* temp = NULL;
+	// Sets the LPWSTR to type char[]
+	int bytSize = WideCharToMultiByte(CP_ACP, 0, coMemResult,
+		-1, 0, 0, 0, 0);
+	temp = new char[bytSize];
+	int retr = WideCharToMultiByte(CP_ACP, 0, coMemResult, -1,
+		temp, bytSize, 0, 0);
+
+	// Sets address of saved equal to the placeholder effectively returning
+	// the recognized string.
+	if (retr != 0)
+	{
+		temp[bytSize - 1] = 0;
+		saved = temp;
+	}
+
+	// Deletes temp allocated memory, cause you know c++.
+	delete[] temp;
+	return 0;
+
+}
+
 // Error handling segment. Checks input address of HRESULTS refrenced against
 // list of common failures. Defualt to common IDK response.
 void ttspeak::checkOut(const HRESULT& hrVal)
 {
-
 	std::string retVal = "";
-
 	switch (hrVal)
 	{
-	case S_OK:
-		return;
-	case E_ABORT:
-		retVal = "Process was aborted.";
-		break;
-	case E_ACCESSDENIED:
-		retVal = "Access was denied.";
-		break;
-	case E_FAIL:
-		retVal = "Process failed.";
-		break;
-	case E_HANDLE:
-		retVal = "Handle was invalid.";
-		break;
-	case E_INVALIDARG:
-		retVal = "Argument was invalid.";
-		break;
-	case E_NOINTERFACE:
-		retVal = "No such interface supported.";
-		break;
-	case E_NOTIMPL:
-		retVal = "Function not implemented.";
-		break;
-	case E_OUTOFMEMORY:
-		retVal = "Out of memory.";
-		break;
-	case E_POINTER:
-		retVal = "Invlaid pointer.";
-		break;
-	case E_UNEXPECTED:
-		retVal = "Unexpected failure.";
-		break;
-	default:
-		retVal = "Unknown Error: " + std::to_string(hrVal);
-	}
+	case S_OK: return;
 
+	case E_ABORT: retVal = "Process was aborted."; break;
+
+	case E_ACCESSDENIED: retVal = "Access was denied."; break;
+
+	case E_FAIL: retVal = "Process failed."; break;
+
+	case E_HANDLE: retVal = "Handle was invalid."; break;
+
+	case E_INVALIDARG: retVal = "Argument was invalid."; break;
+
+	case E_NOINTERFACE:	retVal = "No such interface supported."; break;
+
+	case E_NOTIMPL:	retVal = "Function not implemented."; break;
+
+	case E_OUTOFMEMORY:retVal = "Out of memory."; break;
+
+	case E_POINTER:	retVal = "Invlaid pointer.";break;
+
+	case E_UNEXPECTED:retVal = "Unexpected failure.";break;
+
+	default: retVal = "Unknown Error: " + std::to_string(hrVal);
+	}
 	// Throws exception with output string for clarity and easy debugging.
 	throw std::exception(retVal.c_str());
 	return;
-
 }
